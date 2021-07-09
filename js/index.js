7455766287c3eda24068579650e96fad2b1692a5
Ezihe Godswill <ezihegodswill01@gmail.com>
2021-07-09 13:28:35 +0100

    made header sticky onscroll

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
-document
-    .querySelector(".menu-box")
-    .addEventListener("click", function () {
-        this.classList.toggle("active");
-    });
+const menu = document.querySelector(".menu-box");
+const body = document.body;
+let lastScroll = 0;
+window.addEventListener('scroll', () => {
+    // console.log(window.pageYOffset);
+    const currentScroll = window.pageYOffset;
+    if (currentScroll <= 0) {
+        body.classList.remove('scroll-up');
+    }
+
+    if (currentScroll > lastScroll && !body.classList.contains('scroll-down')) {
+        body.classList.remove('scroll-up');
+        body.classList.add('scroll-down')
+    }
+
+    if (currentScroll < lastScroll && body.classList.contains('scroll-down')) {
+        body.classList.remove('scroll-down');
+        body.classList.add('scroll-up');
+    }
+
+    lastScroll = currentScroll;
+})
+
+menu.addEventListener("click", function () {
+    this.classList.toggle("active");
+});
